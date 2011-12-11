ebc8e3e465f20670244d0e389edac196ad14a741
jinkai.chen <chenjkai@gmail.com>
2011-12-11 15:54:19 +0800

    ex1.0_migrate

---
+++ ex1.0/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ex10::Application.config.secret_token = '4455b492b1641a710cdd4dd27419c7e7687bfcca575c1564d64e94fa28c8a50615179d7c60ee6e2ddb0594de67a428642f902fa2de3cacbfbfad4dabe48c525e'
