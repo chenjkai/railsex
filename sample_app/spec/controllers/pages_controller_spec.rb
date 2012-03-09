1494cf3cf44e4bd864541892fab03f83448b3750
chenjkai <chenjkai@gmail.com>
2012-03-09 10:35:47 +0800

    amend the autotest

--- sample_app/spec/controllers/pages_controller_spec.rb
+++ sample_app/spec/controllers/pages_controller_spec.rb
@@ -22,43 +22,39 @@ describe PagesController do
     end
 
   end
-
   describe "GET 'contact'" do
     it "should be successful" do
-      get 'contact'
-      response.should be_success
+       get 'contact'
+       response.should be_success
     end
     it "should have the right title" do
-        get 'contact'
-        response.should have_selector("title",
-                        :content => "#{@base_title} | Contact")
+      get 'contact' 
+      response.should have_selector("title", :content => "#{@base_title} | Contact")
     end
-
   end
+
 
   describe "GET 'about'" do
     it "should be successful" do
-      get 'about'
-      response.should be_success
+       get 'about'
+       response.should be_success
     end
     it "should have the right title" do
-        get 'about'
-        response.should have_selector("title",
-                        :content => "#{@base_title} | About")
+      get 'about' 
+      response.should have_selector("title", :content => "#{@base_title} | About")
     end
-
   end
 
-describe "GET 'help'" do
+
+  describe "GET 'help'" do
     it "should be successful" do
-      get 'help'
-      response.should be_success
+       get 'help'
+       response.should be_success
     end
     it "should have the right title" do
-        get 'help'
-        response.should have_selector("title",
-                        :content => "#{@base_title} | Help")
+      get 'help' 
+      response.should have_selector("title", :content => "#{@base_title} | Help")
     end
-
+  end
 
 end
