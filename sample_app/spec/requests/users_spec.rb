ea315bdff8e142dc7b1c14583469952f102bca25
chenjkai <chenjkai@gmail.com>
2012-03-18 20:43:32 +0800

    completed signin/out

--- sample_app/spec/requests/users_spec.rb
+++ sample_app/spec/requests/users_spec.rb
@@ -32,4 +32,35 @@ describe "Users" do
       end
     end
   end
+
+
+
+  describe "signin" do
+    describe "failure" do
+      it "should not sign a user in " do
+          visit signin_path
+          fill_in "Email", :with => ""
+          fill_in "Password", :with => ""
+          click_button
+          response.should have_selector('div.flash.error',:content => "Invalid")
+          response.should render_template('sessions/new')
+      end
+    end
+
+    describe "success" do
+      it "should sign a user in or out" do
+          visit signup_path
+          fill_in "Email", :with => "chenjkai1@163.com"
+          fill_in "Password", :with => "steven123"
+          click_button
+          response.should have_selector("div.flash.success",:content => "welcome")
+          response.should render_template('users/show')
+          controller.should be_signed_in
+          click_link "Sign out"
+          controller.should_not be_signed_in
+      end
+    end
+  end
+
+
 end
